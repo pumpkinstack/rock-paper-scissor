0e572ba14505c97d8279719e3ce90cee991688dd
pumpkinstack <284607654+pumpkinstack@users.noreply.github.com>
2026-06-16 14:25:49 +0530

    Add game logic and DOM manipulation

--- script.js
+++ script.js
@@ -1,20 +1,56 @@
+const paper = document.getElementById("paper");
+const rock = document.getElementById("rock");
+const scissors = document.getElementById("scissors");
+const playerChoice = document.getElementById("player-choice-image");
+const computerImage = document.getElementById("computer-choice-image");
+const message = document.getElementById("result-message");
+const score = document.querySelector(".score-value");
+
+let humanScore = 0;
+let computerScore = 0;
+
+rock.addEventListener("click", () => {
+  playerChoice.src = "images/icon-rock.svg";
+  playRound("rock", getComputerChoice());
+});
+
+paper.addEventListener("click", () => {
+  playerChoice.src = "images/icon-paper.svg";
+  playRound("paper", getComputerChoice());
+});
+
+scissors.addEventListener("click", () => {
+  playerChoice.src = "images/icon-scissors.svg";
+  playRound("scissors", getComputerChoice());
+});
+
 function getComputerChoice() {
   const num = Math.floor(Math.random() * 3);
   if (num === 0) {
+    computerImage.src = "images/icon-rock.svg";
     return "rock";
   } else if (num === 1) {
+    computerImage.src = "images/icon-paper.svg";
     return "paper";
   } else {
+    computerImage.src = "images/icon-scissors.svg";
     return "scissors";
   }
 }
 
-function getHumanChoice() {
-  return prompt("Make your Choice: ").toLowerCase();
+function gameOfFive() {
+  if (humanScore === 5) {
+    message.textContent = "You Won The Game!";
+    rock.disabled = true;
+    paper.disabled = true;
+    scissors.disabled = true;
+  } else if (computerScore === 5) {
+    message.textContent = "You Lost The Game!";
+    rock.disabled = true;
+    paper.disabled = true;
+    scissors.disabled = true;
+  }
 }
-
-let humanScore = 0;
-let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
   if (humanChoice === "rock" && computerChoice === "scissors") {
@@ -38,22 +74,18 @@ function playRound(humanChoice, computerChoice) {
   } else {
     console.log("Draw");
   }
+
+  gameOfFive();
+
+  score.textContent = humanScore + " - " + computerScore;
 }
 
-function playGame() {
-  playRound(getHumanChoice(), getComputerChoice());
-  playRound(getHumanChoice(), getComputerChoice());
-  playRound(getHumanChoice(), getComputerChoice());
-  playRound(getHumanChoice(), getComputerChoice());
-  playRound(getHumanChoice(), getComputerChoice());
-
-  if (humanScore > computerScore) {
-    console.log("You Won The Game");
-  } else if (computerScore > humanScore) {
-    console.log("You Lose The Game");
-  } else {
-    console.log("The Game was a Draw");
-  }
-}
-
-playGame();
+document.querySelector(".play-again").addEventListener("click", () => {
+  humanScore = 0;
+  computerScore = 0;
+  score.textContent = "0 - 0";
+  message.textContent = "";
+  rock.disabled = false;
+  paper.disabled = false;
+  scissors.disabled = false;
+});
